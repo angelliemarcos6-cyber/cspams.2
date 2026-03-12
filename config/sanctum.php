f9f9cb43d89d1e39852a0cda082f4c22464f5e47
Your Name <you@example.com>
2026-03-12 16:33:12 +0800

    Adjust login card layout

--- config/sanctum.php
+++ config/sanctum.php
@@ -46,7 +46,7 @@ return [
     |
     */
 
-    'expiration' => env('SANCTUM_TOKEN_EXPIRATION', 15),
+    'expiration' => env('SANCTUM_TOKEN_EXPIRATION', 0),
 
     /*
     |--------------------------------------------------------------------------
